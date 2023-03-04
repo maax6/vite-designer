af5eaaf1c1ba581d69060aab920644627bcee8dc
Maxime <85428733+maax6@users.noreply.github.com>
2023-03-04 18:41:06 +0100

    Active links

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -13,80 +13,79 @@ export function Nav() {
         setActivePage(pageName)
     }
 
-    // function closeMenu() {
-    //     setOpen(!Open)
-    // }
     return (
-        <div className="App">
-            <header className="head">
-                <h1 className="head__logo">This Interior</h1>
-                <ul className={`head__nav ${Open ? 'head__nav--active' : ''}`}>
-                    <li
-                        className={`head__nav__link ${
-                            activePage == 'Home' ? 'head__nav__link--active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/Home"
-                            className="head__nav__link__item"
-                            onClick={() => setActivePage('Home')}
-                        >
-                            {' '}
-                            Home
-                        </a>
-                    </li>
-                    <li
-                        className={`head__nav__link ${
-                            activePage == 'Collection' ? 'head__nav__link__active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/Collection"
-                            className="head__nav__link__item"
-                            onClick={() => setActivePage('Collection')}
-                        >
-                            Collection
-                        </a>
-                    </li>
-                    <li
-                        className={`head__nav__link ${
-                            activePage === 'About' ? 'head__nav__link--active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/About"
-                            className="head__nav__link__item"
-                            onClick={() => setActivePage('About')}
-                        >
-                            About
-                        </a>
-                    </li>
-                    <li
-                        className={`head__nav__link ${
-                            activePage === 'Contact' ? 'head__nav__link--active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/Contact"
-                            className="head__nav__link__item"
-                            onClick={() => setActivePage('Contact')}
-                        >
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-                <div className="head__menuIcon" aria-expanded={Open} onClick={toggleMenu}>
-                    <span
-                        className={`head__menuIcon__burger ${
-                            Open ? 'head__menuIcon__burger--open' : ''
-                        }`}
-                    ></span>
-                    {/* <span className="head__menuIcon__burger"></span>
-                    <span className="head__menuIcon__burger"></span> */}
-                </div>
-            </header>
-            <main className="main"></main>
-            <footer className="footer"></footer>
-        </div>
+       <>
+       
+       <header className="head">
+           <h1 className="head__logo">This Interior</h1>
+           <ul className={`head__nav ${Open ? 'head__nav--active' : ''}`}>
+               <li
+                   className={`head__nav__link ${
+                       activePage === 'Home' ? 'head__nav__link--active' : ''
+                   }`}
+               >
+                   <a
+                       href="/Home"
+                       className="head__nav__link__item"
+                       onClick={(event) => handlePageClick('Home', event)}
+                   >
+                       {' '}
+                       Home
+                   </a>
+               </li>
+               <li
+                   className={`head__nav__link ${
+                       activePage === 'Collection' ? 'head__nav__link--active' : ''
+                   }`}
+               >
+                   <a
+                       href="/Collection"
+                       className="head__nav__link__item"
+                       onClick={(event) => handlePageClick('Collection', event)}
+                   >
+                       Collection
+                   </a>
+               </li>
+               <li
+                   className={`head__nav__link ${
+                       activePage === 'About' ? 'head__nav__link--active' : ''
+                   }`}
+               >
+                   <a
+                       href="/About"
+                       className="head__nav__link__item"
+                       onClick={(event) => handlePageClick('About', event)}
+                   >
+                       About
+                   </a>
+               </li>
+               <li
+                   className={`head__nav__link ${
+                       activePage === 'Contact' ? 'head__nav__link--active' : ''
+                   }`}
+               >
+                   <a
+                       href="/Contact"
+                       className="head__nav__link__item"
+                       onClick={(event) => handlePageClick('Contact', event)}
+                   >
+                       Contact
+                   </a>
+               </li>
+           </ul>
+           <div className="head__menuIcon" aria-expanded={Open} onClick={toggleMenu}>
+               <span
+                   className={`head__menuIcon__burger ${
+                       Open ? 'head__menuIcon__burger--open' : ''
+                   }`}
+               ></span>
+               {/* <span className="head__menuIcon__burger"></span>
+               <span className="head__menuIcon__burger"></span> */}
+           </div>
+       </header>
+       <main className="main"></main>
+       <footer className="footer"></footer>
+       </>
+      
     )
 }
