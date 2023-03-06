137ef5a67a9123da5069ce6af7ace0ff15b2d5c4
Maxime <maxmtlfr@gmail.com>
2023-03-06 13:34:39 +0100

    Welcome left block

--- src/Components/Welcome/index.jsx
+++ src/Components/Welcome/index.jsx
@@ -1,13 +1,24 @@
+import './_welcome.scss'
+import { MdArrowRightAlt } from 'react-icons/md'
+import { IconContext } from 'react-icons'
+
 export function Welcome() {
+
     return (
         <div className="welcome">
-            <div className="welcome__prensentation">
-                <h1 classname="welcome_title">Modern Interior</h1>
+            <div className="welcome__presentation">
+                <h1 className="welcome__title">Modern Interior</h1>
                 <div className="welcome__text">
                     A full-Service residential & commercial interior design and staging company
                     offering professional organizing & eco-services.
                 </div>
-                <span href="/About">Read More</span>
+
+                <a className="welcome__link" href="/About">
+                    Read More
+                    <IconContext.Provider value={{ className: 'welcome__arrow' }}>
+                            <MdArrowRightAlt />
+                    </IconContext.Provider>
+                </a>
             </div>
         </div>
     )
