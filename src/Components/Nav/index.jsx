f45c0a831cd6145371d54212dbf43550b2f7f157
Maxime S <Maxmtlfr@gmail.com>
2023-03-12 17:02:05 +0100

    Fix React router Nav Active

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,67 +1,91 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import './_nav.scss'
 
 export function Nav() {
-  const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+    const [activePage, setActive] = useState('Home')
 
-  function toggleMenu() {
-    setIsOpen(prevOpen => !prevOpen)
-  }
+    function toggleMenu() {
+        setIsOpen((prevOpen) => !prevOpen)
+    }
 
-  return (
-    <>
-      <header className="head">
-        <h1 className="head__logo">This Interior</h1>
-        <ul className={`head__nav ${isOpen ? 'head__nav--active' : ''}`}>
-          <li className="head__nav__item">
-            <NavLink
-              to="/Home"
-              activeClassName="head__nav__item__link--active"
-              className="head__nav__item__link"
-              onClick={() => setIsOpen(false)}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="head__nav__item">
-            <NavLink
-              to="/Collection"
-              activeClassName="head__nav__item__link--active"
-              className="head__nav__item__link"
-              onClick={() => setIsOpen(false)}
-            >
-              Collection
-            </NavLink>
-          </li>
-          <li className="head__nav__item">
-            <NavLink
-              to="/About"
-              activeClassName="head__nav__item__link--active"
-              className="head__nav__item__link"
-              onClick={() => setIsOpen(false)}
-            >
-              About
-            </NavLink>
-          </li>
-          <li className="head__nav__item">
-            <NavLink
-              to="/Contact"
-              activeClassName="head__nav__item__link--active"
-              className="head__nav__item__link"
-              onClick={() => setIsOpen(false)}
-            >
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-        <div
-          className={`head__menuIcon ${isOpen ? 'head__menuIcon--open' : ''}`}
-          aria-expanded={isOpen}
-          onClick={toggleMenu}
-        >
-          <span className="head__menuIcon__burger"></span>
-        </div>
-      </header>
-    </>
-  )
+    return (
+        <>
+            <header className="head">
+                <h1 className="head__logo">This Interior</h1>
+                <ul className={`head__nav ${isOpen ? 'head__nav--active' : ''}`}>
+                    <li className="head__nav__item">
+                        <NavLink
+                            to="/Home"
+                            className={`head__nav__item__link ${
+                                activePage === 'Home' ? 'head__nav__item__link--active' : ''
+                            }`}
+                            onClick={() => {
+                                setActive('Home')
+                                setIsOpen(false)
+                            }}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li
+                        className={`head__nav__item ${
+                            activePage === 'Collection' ? 'head__nav__item--active' : ''
+                        }`}
+                    >
+                        <NavLink
+                            to="/Collection"
+                            className="head__nav__item__link"
+                            onClick={() => {
+                                setActive('Collection')
+                                setIsOpen(false)
+                            }}
+                        >
+                            Collection
+                        </NavLink>
+                    </li>
+                    <li
+                        className={`head__nav__item ${
+                            activePage === 'About' ? 'head__nav__item--active' : ''
+                        }`}
+                    >
+                        <NavLink
+                            to="/About"
+                            className="head__nav__item__link"
+                            onClick={() => {
+                                setActive('About')
+                                setIsOpen(false)
+                            }}
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                    <li
+                        className={`head__nav__item ${
+                            activePage === 'Contact' ? 'head__nav__item--active' : ''
+                        }`}
+                    >
+                        <NavLink
+                            to="/Contact"
+                            className="head__nav__item__link"
+                            onClick={() => {
+                                setActive('Contact')
+                                setIsOpen(false)
+                            }}
+                        >
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+                <div
+                    className={`head__menuIcon ${isOpen ? 'head__menuIcon--open' : ''}`}
+                    aria-expanded={isOpen}
+                    onClick={toggleMenu}
+                >
+                    <span className="head__menuIcon__burger"></span>
+                </div>
+            </header>
+        </>
+    )
 }
