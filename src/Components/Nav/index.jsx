01a2bd2ef33c6cf2424b7e488462256ce63ef984
Maxime S <Maxmtlfr@gmail.com>
2023-03-12 16:43:02 +0100

    Recup nav

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,85 +1,69 @@
-import { useState } from 'react'
+	import './_nav.scss'
+	import { useState } from 'react'
+	import { NavLink } from 'react-router-dom'
 
 export function Nav() {
-    const [Open, setOpen] = useState(false)
-    const [activePage, setActivePage] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
 
-    function toggleMenu() {
-        setOpen((prevOpen) => !prevOpen)
-    }
+  function toggleMenu() {
+    setIsOpen(prevOpen => !prevOpen)
+  }
 
-    function handlePageClick(pageName, event) {
-        
-        setActivePage(pageName)
-    }
-
-    return (
-        <>
-            <header className="head">
-                <h1 className="head__logo">This Interior</h1>
-                <ul className={`head__nav ${Open ? 'head__nav--active' : ''}`}>
-                    <li
-                        className={`head__nav__link ${
-                            activePage === 'Home' ? 'head__nav__link--active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/Home"
-                            className="head__nav__link__item"
-                            onClick={(event) => handlePageClick('Home', event)}
-                        >
-                            {' '}
-                            Home
-                        </a>
-                    </li>
-                    <li
-                        className={`head__nav__link ${
-                            activePage === 'Collection' ? 'head__nav__link--active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/Collection"
-                            className="head__nav__link__item"
-                            onClick={(event) => handlePageClick('Collection', event)}
-                        >
-                            Collection
-                        </a>
-                    </li>
-                    <li
-                        className={`head__nav__link ${
-                            activePage === 'About' ? 'head__nav__link--active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/About"
-                            className="head__nav__link__item"
-                            onClick={(event) => handlePageClick('About', event)}
-                        >
-                            About
-                        </a>
-                    </li>
-                    <li
-                        className={`head__nav__link ${
-                            activePage === 'Contact' ? 'head__nav__link--active' : ''
-                        }`}
-                    >
-                        <a
-                            href="/Contact"
-                            className="head__nav__link__item"
-                            onClick={(event) => handlePageClick('Contact', event)}
-                        >
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-                <div className="head__menuIcon" aria-expanded={Open} onClick={toggleMenu}>
-                    <span
-                        className={`head__menuIcon__burger ${
-                            Open ? 'head__menuIcon__burger--open' : ''
-                        }`}
-                    ></span>
-                </div>
-            </header>
-        </>
-    )
+  return (
+    <>
+      <header className="head">
+        <h1 className="head__logo">This Interior</h1>
+        <ul className={`head__nav ${isOpen ? 'head__nav--active' : ''}`}>
+          <li className="head__nav__item">
+            <NavLink
+              to="/Home"
+              activeClassName="head__nav__item__link--active"
+              className="head__nav__item__link"
+              onClick={() => setIsOpen(false)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="head__nav__item">
+            <NavLink
+              to="/Collection"
+              activeClassName="head__nav__item__link--active"
+              className="head__nav__item__link"
+              onClick={() => setIsOpen(false)}
+            >
+              Collection
+            </NavLink>
+          </li>
+          <li className="head__nav__item">
+            <NavLink
+              to="/About"
+              activeClassName="head__nav__item__link--active"
+              className="head__nav__item__link"
+              onClick={() => setIsOpen(false)}
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="head__nav__item">
+            <NavLink
+              to="/Contact"
+              className="head__nav__item__link"
+              onClick={() => setIsOpen(false)}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <div
+          className={`head__menuIcon ${isOpen ? 'head__menuIcon--open' : ''}`}
+          aria-expanded={isOpen}
+          onClick={toggleMenu}
+        >
+          <span className="head__menuIcon__burger"></span>
+        </div>
+      </header>
+    </>
+  ) 
 }
+
+		
