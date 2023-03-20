5c2a26789221173b0117304f22b48e279415c1fe
Maxime <maxmtlfr@gmail.com>
2023-03-20 14:47:04 +0100

    test vercel

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { Home } from '../Pages/Home'
 import { Error } from '../Components/Error'
+import { Home } from '../Pages/Home'
 import{ LayoutDefault } from '../layouts/Default'
 
 export function Router() {
