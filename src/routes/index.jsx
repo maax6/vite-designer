6eeb26212f597178d8ef19230fe995309e38c3b3
Maxime <maxmtlfr@gmail.com>
2023-03-21 19:07:36 +0100

    Fixed maj

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Error } from '../Components/Error'
 import { Home } from '../pages/Home/Home'
-import{ LayoutDefault } from '../layouts'
+import{ LayoutDefault } from '../Layouts/Default'
 
 export function Router() {
    return (
