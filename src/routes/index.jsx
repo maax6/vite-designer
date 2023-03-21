057eaee6b7d658661da4f204d3eecebb1da5146b
Maxime <maxmtlfr@gmail.com>
2023-03-21 19:06:10 +0100

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Error } from '../Components/Error'
 import { Home } from '../pages/Home/Home'
-import{ LayoutDefault } from '/src/layouts/Default.jsx'
+import{ LayoutDefault } from '../layouts'
 
 export function Router() {
    return (
